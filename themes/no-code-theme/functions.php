8c4398c818904a30c779d28806ab9cbb803e8a7a
JadeB27 <jadesbedlington@gmail.com>
2025-02-25 13:44:41 -0500

    had to add WooCommerce back after it deleted after adding GutenBerg

--- themes/no-code-theme/functions.php
+++ themes/no-code-theme/functions.php
@@ -8,6 +8,11 @@
  * @since 1.0.0
  */
 
+ function enqueue_custom_woocommerce_styles() {
+    wp_enqueue_style('custom-woocommerce', get_stylesheet_directory_uri() . '/custom-woocommerce.css');
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_woocommerce_styles');
+
 //  Hooks.
 require_once get_theme_file_path('inc/hooks.php');
 
